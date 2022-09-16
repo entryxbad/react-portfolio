7ca8149dc1d32df299ca64f14a54b31bd41dcb4f
Radmir <radmir_96@mail.ru>
2022-09-16 16:35:05 +0500

    fix

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -4,16 +4,16 @@ import { title } from '../../utils/Tools/Tools'
 const About = () => {
     return (
         <div className='flex items-center flex-col pt-10 mb-10 px-10'>
-            <div className={title}>About me</div>
+            <div className={title}>Обо мне</div>
             <div className='flex flex-col items-center text-center text-base sm:text-lg lg:text-2xl'>
                 <p>
-                    Hi, my name is Radmir. Front-end developer from Ufa. I am interested in web-development and everything connected with it.
+                    Привет, меня зовут Радмир. Front-end разработчик из Уфы. Интересуюсь веб-разработкой и всем, что с ней связано.
                 </p>
                 <p>
-                    I have higher technical education. Also trained in web development courses.
+                    Имею высшее техническое образование. Также обучался на курсах веб-разработки.
                 </p>
                 <p>
-                    Ready to do great projects with great people.
+                    Готов реализовать отличные проекты с замечательными людьми.
                 </p>
             </div>
         </div>
--- src/component/Skills/Skills.jsx
+++ src/component/Skills/Skills.jsx
@@ -6,8 +6,8 @@ const Skills = () => {
     return (
         <div className='h-96' >
             <div className='flex items-center flex-col'>
-                <div className={title}>My skills</div>
-                <div className='text-base mb-10 sm:text-lg lg:text-2xl'>I can work with</div>
+                <div className={title}>Мои навыки</div>
+                <div className='text-base mb-10 sm:text-lg lg:text-2xl'>Я умею работать с</div>
             </div>
             <SimpleSlider />
         </div>
