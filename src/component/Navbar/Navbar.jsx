a74a8e31337ffc9e1cb0f3113e60f3663a25678a
Radmir <radmir_96@mail.ru>
2022-10-05 21:17:24 +0500

    logo was changed

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
             <div>
-                <img src={Logo} alt="logo" style={{ width: '80px' }} />
+                <img className='animate-pulse' src={Logo} alt="logo" style={{ width: '80px' }} />
             </div>
 
             {/* menu */}
